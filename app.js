cef02c74e457cea9c3e37c8d176bb1c259eaca86
kpiekut <karol.piekut@pm.me>
2024-08-02 13:26:19 +0100

    add task display fix

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const projectStatusInput = document.querySelector("#projectStatus");
 const addProjectButton = document.querySelector("#addProject");
 const activeProjectsList = document.querySelector("#activeProjects");
 const archiveProjects = document.querySelector("#archivedProjectsSelect");
-
+const allProjectsSelect = document.querySelector("#allProjectsSelect");
 const taskNameInput = document.querySelector("#taskName");
 const taskDateInput = document.querySelector("#taskDate");
 const taskStatusInput = document.querySelector("#taskStatus");
 const addTaskButton = document.querySelector("#addTask");
+const taskListSection = document.querySelector("#taskList");
+const tasksDomContainer = document.querySelector("#tasksDOMContainer");
 
 let appStorage = localStorage.getItem("appStorage") ?
     JSON.parse(localStorage.getItem('appStorage')) : [];
@@ -68,9 +70,62 @@ function createAProjectDom(projectName) {
     activeProjectsList.appendChild(projectItem);
 }
 
-function displayTasksDom(selectedProject){
+function displayIndividualProjectTasks() {
 
 }
+
+
+function displayTasksDom(selectedProject) {
+    while (tasksDomContainer.hasChildNodes()) {
+        tasksDomContainer.removeChild(tasksDomContainer.firstChild);
+    }
+    if (selectedProject === "allProjectsSelect") {
+        console.log("you have selected all projects");
+    } else if (selectedProject === "archivedProjectsSelect") {
+        console.log("you have selected all archived projects");
+    } else {
+        for (let i in appStorage[selectedProject].taskList) {
+            let taskRepeat =
+                `<div class="individualTask">
+                    <p class="taskNameClass">${appStorage[selectedProject].taskList[i].taskName}</p>
+                    <time class="taskDateClass" dateTime="2024-06-27">${appStorage[selectedProject].taskList[i].date}</time>
+                    <p class="taskStatusClass">${appStorage[selectedProject].taskList[i].status}</p>
+                    <div class="taskButtons">
+                        <button class="taskCompleteButtonClass"><?xml version="1.0" encoding="UTF-8"?>
+                            <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg" color="#FFFFFFFF">
+                                <path d="M5 13L9 17L19 7" stroke="#FFFFFFFF" stroke-width="1.5" stroke-linecap="round"
+                                      stroke-linejoin="round"></path>
+                            </svg>
+                        </button>
+                        <button class="taskDeleteButtonClass"><?xml version="1.0" encoding="UTF-8"?>
+                            <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none"
+                                 xmlns="http://www.w3.org/2000/svg" color="#FFFFFFFF">
+                                <path
+                                    d="M20 9L18.005 20.3463C17.8369 21.3026 17.0062 22 16.0353 22H7.96474C6.99379 22 6.1631 21.3026 5.99496 20.3463L4 9"
+                                    stroke="#FFFFFFFF" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                <path
+                                    d="M21 6L15.375 6M3 6L8.625 6M8.625 6V4C8.625 2.89543 9.52043 2 10.625 2H13.375C14.4796 2 15.375 2.89543 15.375 4V6M8.625 6L15.375 6"
+                                    stroke="#FFFFFFFF" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>`
+            // taskListSection.appendChild(taskRepeat);
+            tasksDomContainer.insertAdjacentHTML( 'beforeend', taskRepeat );
+            console.log(appStorage[selectedProject].taskList[i].date);
+        }
+        //clear dom with each select
+        //find a way to insert variables
+        console.log(`you have selected ${selectedProject}`);
+    }
+}
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//FIX TASK SCROLL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 function Task(taskName, date, status) {
     customTaskIndex++;
@@ -128,15 +183,16 @@ activeProjectsList.addEventListener('click', function (event) {
     if(!event.target.classList.contains("projectListSelect")) return;
     // let clickedItem = event.target
     projectSelectedState = event.target.id;
-    console.log(projectSelectedState);
+    displayTasksDom(projectSelectedState);
 })
 
 archiveProjects.addEventListener("click", function (event) {
     projectSelectedState = event.target.id;
-    console.log(projectSelectedState);
+    displayTasksDom(projectSelectedState);
 })
 
-
-
-
+allProjectsSelect.addEventListener("click", function (event) {
+    projectSelectedState = event.target.id;
+    displayTasksDom(projectSelectedState);
+})
 
